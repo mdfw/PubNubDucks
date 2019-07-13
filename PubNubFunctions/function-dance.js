27c6d7d6d4ae192fd9ee99b3382888b1d45f3020
Mark Williams <mdfw@mdfw@users.noreply.github.com>
2019-07-13 08:49:01 -0700

    Add documentation.

--- PubNubFunctions/function-dance.js
+++ PubNubFunctions/function-dance.js
@@ -1,13 +1,21 @@
 /*
-    PubNub Functions Pirate Duck Bot
+    Pirate Duck Chat Demo - Dance move function
     Copyright 2019, Mark D. F. Williams, All rights reserved
 
-    Part of the PubNub Pirate Duck Chat Demo project.     
-
+    PubNub Function for demonstrating "After Publish or Fire" event type and the KV Store.
+    1. Listens to all messages on the ducks.color and ducks.talk channels.
+    2. When a new message is received, the previous messages are accessed from the KV store.
+    3. Matches the most recent 3 messages against the defined secrets
+    4. If a match is made, a message is sent through the dance channel and then an empty set is stored in the KV store.
+    5. If a match is not made, the last 3 sent messages are stored in the KV store.
 */
 
-// Woof!, Red, Woof!
-const DANCE_KEY = "duck_dance_key";
+const CHANNEL_NAME_COLOR = 'ducks.color';
+const CHANNEL_NAME_TALK = 'ducks.talk';
+const CHANNEL_NAME_DANCE = 'ducks.dance';
+const CHANNEL_KEY_DANCE = "style";
+const KV_KEY_DANCE = "duck_dance_key";
+
 const DEFINED_SECRET_SETS = [{
             // Blue, Yellow, Red
             secret: ["color:#4421D0", "color:#FECE53", "color:#D02129"],
@@ -17,68 +25,72 @@ const DEFINED_SECRET_SETS = [{
             secret: ["color:#FECE53", "color:#D02129", "color:#4421D0"],
             dance: "shake"
         }, {
-            // Quacks... , Blue, Woof!(Quack!)
-            secret: ["text:If it quacks like a duck, is it a duck?", "color:#4421D0", "text:Quack!"],
+            // Shiver , Blue, Woof!(Quack!)
+            secret: ["text:Shiver me tail feathers!", "color:#4421D0", "text:Quack!"],
             dance: "swing"
         }, {
-            // Yellow
-            secret: ["color:#FECE53", "text:A duck without Ahab is just a duck.", "text:Quack!"],
+            // Red, Duckie, Red
+            secret: ["color:#D02129", "text:Duckie was robbed!", "color:#D02129"],
             dance: "tada"
+        }, {
+            // Quack! Quack! Quack!
+            secret: ["text:Quack!", "text:Quack!", "text:Quack!"],
+            dance: "lightSpeedOut"
         }
     ];
 
 const pubnub = require('pubnub');
 
+/** 
+ * The core function that is called by the block. 
+ * 'Request' contains the information we need to make decisions about our dance.
+ */
 export default (request) => { 
-    if (!(request.channels.includes("ducktalk") || request.channels.includes("duckcolor"))) {
+    // If the channels are not the talk or color channels, ignore the event and return the resolved promise.
+    if (!(request.channels.includes(CHANNEL_NAME_TALK) || request.channels.includes(CHANNEL_NAME_COLOR))) {
         return request.ok();
     }
+    // Load the kvstore
     const kvstore = require('kvstore');
+
     let currentStep = deriveCurrentStep(request);
     if (!currentStep) {
-        //console.log("Skipping - no step.");
         return request.ok();
-
     } else {
-        return kvstore.get(DANCE_KEY).then((storedSteps) => {
+        return kvstore.get(KV_KEY_DANCE).then((storedSteps) => {
             // Build out the steps
-            //console.log("1. StoredSteps: " + JSON.stringify(storedSteps));
             return buildSteps(storedSteps, currentStep);
         }).then((steps) => {
             // Test the steps
-            //console.log("2: " + JSON.stringify(steps));
             return matchDanceMoves(steps);
- 
         }).then((steps) => {
             // Save
-            //console.log("3: " + JSON.stringify(steps));
-            kvstore.set(DANCE_KEY, steps);
+            kvstore.set(KV_KEY_DANCE, steps);
             return request.ok();
         });
     }
-    return request.ok(); // Return a promise when you're done 
+    return request.ok();
 }
 
 function deriveCurrentStep(request) {
     let currentStep = null;
-    if (request.channels.includes("ducktalk")) {
+    if (request.channels.includes(CHANNEL_NAME_TALK)) {
         let speech = request.message.text;
         if (speech) {
             currentStep = "text:" + speech;
         }
-    } else if (request.channels.includes("duckcolor")) {
+    } else if (request.channels.includes(CHANNEL_NAME_COLOR)) {
         let color = request.message.color;
         if (color) {
             currentStep = "color:" + color;
         }
     }
-    //console.log("currentStep :: " + currentStep);
     return currentStep;
 }
 
+
 function buildSteps(inStoredSteps, inCurrentStep) {
     let steps = null;
-    //console.log("inStoredSteps", JSON.stringify(inStoredSteps));
     if (inStoredSteps) {
         steps = inStoredSteps
     } else {
@@ -88,7 +100,6 @@ function buildSteps(inStoredSteps, inCurrentStep) {
     if (steps.length > 3) {
         let removed = steps.shift();
     }
-    //console.log("New steps..." + JSON.stringify(steps));
     return steps;
 }
 
@@ -99,21 +110,17 @@ function matchDanceMoves(steps) {
     for (let i = 0; i < DEFINED_SECRET_SETS.length; i++) {
         let thisSet = DEFINED_SECRET_SETS[i];
         let thisSecret = thisSet.secret;
-        //console.log("Starting match for :" + JSON.stringify(thisSecret));
         let didAllMatch = false;
 
         for (let j = 0; j < thisSecret.length; j++) {
             didAllMatch = true;
             if (thisSecret[j] !== steps[j]) {
-                //console.log("[" + i + "][" + j +"] NO match: " + thisSecret[j] + " and " + steps[j]);
                didAllMatch = false;
                break;
             } else {
-                //console.log("[" + i + "][" + j +"] matched: " + thisSecret[j] + " and " + steps[j]);
             }
         }
         if (didAllMatch) {
-            //console.log("Found a match for " + JSON.stringify(steps) + " ::" + JSON.stringify(thisSet));
             didMatchASecretSet = true;
             matchedDanceMove = thisSet.dance;
             break;
@@ -121,23 +128,19 @@ function matchDanceMoves(steps) {
     }
     
     if (didMatchASecretSet) {
-        // console.log("Tried to dance - and will send " + matchedDanceMove);
         return callTheDance(matchedDanceMove);
     } else {
-        //console.log("Tried to dance - and did not");
         return steps;
     }
 }
 
 function callTheDance(danceMove) {
     return pubnub.publish({
-        "channel": "duckdance",
+        "channel": CHANNEL_NAME_DANCE,
         "message": {
-            "style": danceMove
+            [CHANNEL_KEY_DANCE]: danceMove
         }
     }).then((publishResponse) => {
-        //console.log(`Publish Status: ${publishResponse[0]}:${publishResponse[1]} with TT ${publishResponse[2]}`);
         return [];
     });
-
 }
