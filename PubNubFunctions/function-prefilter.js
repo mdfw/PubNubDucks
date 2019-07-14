be367e8ab7379713a81a3b09eef7ae8b013f20ee
Mark Williams <mdfw@mdfw@users.noreply.github.com>
2019-07-13 21:28:37 -0700

    Remove filtering of non-woof messages. Also added more documentation.

--- PubNubFunctions/function-prefilter.js
+++ PubNubFunctions/function-prefilter.js
@@ -2,68 +2,49 @@
     PubNub Functions Pirate Duck Bot
     Copyright 2019, Mark D. F. Williams, All rights reserved
 
-    Part of the PubNub Pirate Duck Chat Demo project.     
-
+    Part of the PubNub Pirate Duck Chat Demo project.
+    Shows two possible interceptions. One is a bad word filter, the other a translation filter. For ducks. 
+    Install as a "Before publish or subscribe message" type Function.
 */
 
-const allMessages = [{buttonText: "Down!",
-        fullText: "Down with the Geese!"},
-        {buttonText: "Quack…?",
-        fullText: "If I quack like a duck, am I a duck?"},
-        {buttonText: "Ducky…",
-        fullText: "Duckie was robbed!"},
-        {buttonText: "Strait…",
-        fullText: "Messier 11 and strait on 'til morning."},
-        {buttonText: "Ugly?",
-        fullText: "Ugly Duckling? The swans wish they could be so cool."},
-        {buttonText: "Betrayal",
-        fullText: "A friend may betray you, but a duck will always stay the same."},
-        {buttonText: "Where…",
-        fullText: "Where there is a pond, there are Pirate Ducks."},
-        {buttonText: "Shiver",
-        fullText: "Shiver me tail feathers!"},
-        {buttonText: "Good night",
-        fullText: "Good night, Duckies. Good work. Sleep well. I'll most likely kill you in the morning."},
-        ]
-
-
-// Declare the Function with the export syntax. The incoming message is called request
+/** 
+ * The main function is declared with the export syntax. The incoming message is called request.
+ * Our function first calls a language filter then a translation. Both modify and return the request object.
+ * Be careful with "before publish" functions. They block delivery until the promise is resolved.
+ * Although these functions return quickly because all logic is local, if you do call out to external
+ *   helpers (see PubNub Blocks catalog), your external call must finish before delivery continues.
+ */
 export default (request) => {
     // Require console module to display variables for troubleshooting
-    var console = require("console");
+    // var console = require("console");
     return filter(request).then(translate).then(function(result) {
-        //console.log(result);
         return result;
     })
 };
 
+/**
+ * Filter the request message for any bad words in the Pirate Duck language.
+ * @param {*} request 
+ */
 let filter = function(request) {
     return new Promise(function(resolve, reject) {
-        console.log(request.message);
-        const requestMessageText = request.message.text;
+        //console.log(request.message);
 
         // If the text field in the message is "Woof!", we change it to "Quack!"
-        if (requestMessageText === "Woof!") {
+        // We also add a note to the message that could be optionally shown to the user.
+        if (request.message.text === "Woof!") {
             request.message.text = "Quack!";
             request.message.note = "Note: Content was changed to align with community standards."
             return resolve(request);
-        }
-        // If the message text does not equal one of our accepted texts, change it.
-        let badphrase = true;
-        for (const msg of allMessages) {
-            if (msg.fullText === requestMessageText) {
-                badphrase = false;
-                break;
-            }
-        }
-        if (badphrase) {
-            request.message.text = "Quack?"
-            request.message.note = "Note: Content was changed as it was not in the approved list."         
         }
         resolve(request);
     });
 }
 
+/**
+ * Translate the message to morse code. This isn't used fully in our demo yet, but we have plans.
+ * @param {*} request 
+ */
 let translate = function(request) {
     return new Promise(function(resolve, reject) {
         if (request.message.text === "Quack!") {
