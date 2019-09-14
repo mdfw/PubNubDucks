797e01bb17b359e979f3899c2bb586b4c170770b
Mark Williams <mdfw@mdfw@users.noreply.github.com>
2019-09-13 18:43:11 -0700

    Add test code for web notifications

--- js/index.js
+++ js/index.js
@@ -596,3 +596,30 @@ function startCloudDucks () {
 function stopCloudDucks () {
     clearInterval(cloudDuckInterval);
 }
+
+function notifyMe(speech, publisher) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support system notifications");
+      // This is not how you would really do things if they aren't supported. :)
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(publisher + ": " + speech);
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+            var notification = new Notification(publisher + ": " + speech);
+        }
+      });
+    }
+  
+    // Finally, if the user has denied notifications and you 
+    // want to be respectful there is no need to bother them any more.
+  }
